7e281a66a84ffb0def03f5f79a124955e10b563f
hanashiro <felipeaah@gmail.com>
2021-11-18 13:04:22 -0300

    aula-02-04

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import styled from '@emotion/styled';
+import styled from '@emotion/styled/macro';
 import { useState } from 'react';
+
+const MeuSpan = styled('span')``;
 
 const MinhaDiv = styled('div')`
     background-color: green;
     color: ${({ ligado }) => (ligado ? 'white' : 'red')};
+
+    &:hover ${MeuSpan} {
+        color: red;
+    }
 `;
 
 function App() {
     const [ligado, setLigado] = useState(true);
     return (
         <div onClick={() => setLigado(!ligado)}>
-            <MinhaDiv ligado={ligado}>TreinaWeb</MinhaDiv>
+            <MinhaDiv ligado={ligado}>
+                TreinaWeb
+                <MeuSpan>Teste</MeuSpan>
+            </MinhaDiv>
         </div>
     );
 }
