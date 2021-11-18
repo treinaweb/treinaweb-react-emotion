938b5d9843ac311b5b0b1f0977bdfde7d44dcb13
hanashiro <felipeaah@gmail.com>
2021-11-18 11:54:03 -0300

    aula-02-02

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled';
+
+const MinhaDiv = styled('div')`
+    background-color: green;
+    color: white;
+`;
+
+const MinhaOutraDiv = styled(MinhaDiv)`
+    font-weight: bold;
+    color: red;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <MinhaDiv>TreinaWeb</MinhaDiv>
+            <MinhaOutraDiv>TreinaWeb 222</MinhaOutraDiv>
+        </div>
+    );
 }
 
 export default App;
